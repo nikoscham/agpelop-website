ecf751f0912033467bb37f393c53afe29fafadec
nikoscham <nikoscham@gmail.com>
2024-12-21 17:39:37 +0200

    Enhance bridge selection and details display with additional data and improved layout

--- agpelop-archive.js
+++ agpelop-archive.js
@@ -1,42 +1,91 @@
 const bridgeData = {
-    'gefyri-1': {
-        name: 'Γεφύρι της Καρύταινας',
-        county: 'Αρκαδία',
-        county2: 'Αρκαδίας',
-        municipal: '-',
-        river: 'Αλφειός',
-        location: '-',
-        year: '1441',
-        builder: '-',
-        description: 'Αποτελείται από πέντε άνισα τόξα με ορθογώνια ανακουφιστικά ανοίγματα, τρία επάλληλα καταστρώματα, έχει ύψος 12 μέτρα, μήκος 50 και το μεγαλύτερο τόξο έχει άνοιγμα 8,75 μέτρα.',
-        links: 'Σχετικό άρθρο στο blog: <a href="http://agpelop.blogspot.com/2013/09/normal-0-false-false-false-el-x-none-x_2.html" target="_blank">Το γεφύρι της Καρύταινας </a>',
-    },
-    'gefyri-2': {
-        name: 'Γεφύρι της Μαυροζούμενας',
-        county: 'Μεσσηνία',
-        county2: 'Μεσσηνίας',
-        municipal: '-',
-        river: '-',
-        location: '-',
-        year: '-',
-        builder: '-',
-        description: '-',
-        links: '-',
-    }
+  "gefyri-1": {
+    name: "Γεφύρι της Καρύταινας",
+    county: "Αρκαδίας",
+    municipal: "Μεγαλόπολης",
+    river: "Αλφειός",
+    location: "Κάτω από την Καρύταινα",
+    year: "1441",
+    builder: "Άγνωστος",
+    description:
+      "Αποτελείται από πέντε άνισα τόξα με ορθογώνια ανακουφιστικά ανοίγματα, τρία επάλληλα καταστρώματα, έχει ύψος 12 μέτρα, μήκος 50 και το μεγαλύτερο τόξο έχει άνοιγμα 8,75 μέτρα.",
+    links:
+      'Σχετικό άρθρο στο blog: <a href="http://agpelop.blogspot.com/2013/09/normal-0-false-false-false-el-x-none-x_2.html" target="_blank">Το γεφύρι της Καρύταινας</a>',
+  },
+  "gefyri-2": {
+    name: "Γεφύρι της Μαυροζούμενας",
+    county: "Μεσσηνίας",
+    municipal: "Οιχαλίας",
+    river: "-",
+    location: "Έξοδος Νεοχωρίου προς Μελιγαλά",
+    year: "-",
+    builder: "Άγνωστος",
+    description: "-",
+    links: "-",
+  },
+  "gefyri-3": {
+    name: "Τουρκογέφυρο στην Πρόσυμνα Αργολίδας",
+    county: "Αργολίδας",
+    municipal: "Άργους-Μυκηνών",
+    river: "Ρέμα Ταξιάρχη",
+    location: "Πάνω από το χωριό Πρόσυμνα",
+    year: "Τουρκοκρατία",
+    builder: "Άγνωστος",
+    description:
+      "Βρίσκεται πάνω από το χωριό Πρόσυμνα (Μπερμπάτι) και είναι μονότοξο, με μια σειρά θολίτες και χωρίς στηθαία. Πρόκειται για ένα πανέμορφο πετρογέφυρο, κοντά στην αρχαία Κοντοπορεία οδό, με την επιφάνειά του να σχηματίζει κύκλο και να μεγαλώνει στα άκρα του. Οι διαστάσεις του είναι: Ύψος: 4 μ. πλάτος: 2,50 μ. άνοιγμα καμάρας: 3 μ. μήκος: 10.70 μ. και μήκος θολιτών: 0,25 μ",
+    links: 'Σχετικό άρθρο στο blog: <a href="http://agpelop.blogspot.com/2015/01/blog-post.html?spref=bl" target="_blank">Τουρκογέφυρο στην Πρόσυμνα Αργολίδας</a>',
+  },
+  "gefyri-4": {
+    name: "Γεφύρι στη θέση “Δομοκός” ή Βιδιακίτικο γεφύρι",
+    county: "Αρκαδίας",
+    municipal: "Γορτυνίας",
+    river: "Ερύμανθος",
+    location: "«Δομοκός»",
+    year: "1870",
+    builder: "Αφοί Φώτης, Κώστας, Πάνος και Σπύρος Πουρνάρας",
+    description:
+      "Λέγεται και Βιδιακίτικο λόγω της θέσης του κοντά στο χωριό Βιδιάκι Γορτυνίας, πού βρίσκεται δυτικά σε απόσταση τριών χιλιομέτρων και που είναι χτισμένο στη θέση της αρχαίας πόλης Στράτος. Χτίστηκε το 1870 (κατ' άλλους το 1875) από ντόπιους, Λαγκαδινούς στην καταγωγή μαστόρους, τους αδελφούς Φώτη, Κώστα, Πάνο και Σπύρο Πουρνάρα από το Μοναστηράκι, που έχτισαν το “Παραλογγίτικο” επί του Ερύμανθου επίσης και το γεφύρι της “Δήμητρας” κοντά στο ομώνυμο χωριό - πρώην Δίβριτσα - της Γορτυνίας, επί του Λάδωνα.",
+    links: 'Σχετικό άρθρο στο blog: <a href="http://agpelop.blogspot.com/2016/10/blog-post_10.html?spref=bl" target="_blank">Γεφύρι στη θέση “Δομοκός” ή Βιδιακίτικο γεφύρι</a>',
+  },
+  "gefyri-5": {
+    name: "Γεφύρι της Κυράς",
+    county: "Αρκαδίας",
+    municipal: "Γορτυνίας",
+    river: "Λάδωνας",
+    location: "Μεταξύ Μυγδαλιάς-Πουρναριάς και Μουριάς",
+    year: "Τα δύο πρώτα τόξα επί φραγκοκρατίας, τα άλλα δύο επί τουρκοκρατίας και το τρίτο το 1908",
+    builder:
+      "Των τεσσάρων πρώτων τόξων άγνωστοι, το πέμπτο ο Λαγκαδινός Ανδρικόπουλος",
+    description:
+      "Το συνολικό της μήκος είναι 56,20 μ.η επιφάνεια διάβασης επίπεδη στην οποία διακρίνονται ίχνη καλντεριμιού, το πλάτος των στηθαίων είναι 0,54 μ. και έχει γίνει προσθήκη τσιμέντου 5 περίπου εκατοστών. Το ύψος του στηθαίου διαφέρει, μεγαλώνοντας όσο πάμε προς το κέντρο του γεφυριού (από 0,38 μέχρι 0,70 μ. στο κεντρικό τόξο). Το καθαρό πλάτος του γεφυριού είναι 1,95 μ.και τα τόξα του είναι άνισα και καταβιβασμένα. Πρόκειται για ένα εντυπωσιακό πεντάτοξο γεφύρι σε μια θαυμάσια και ιστορική περιοχή.",
+    links: 'Σχετικό άρθρο στο blog: <a href="http://agpelop.blogspot.com/2019/09/blog-post.html" target="_blank">Το γεφύρι της "Κυράς" στον Λάδωνα</a>',
+  },
+  "gefyri-6": {
+    name: "Γεφύρι Τσερχοτάμπεη",
+    county: "Αχαΐας",
+    municipal: "Καλαβρύτων",
+    river: "Λάδωνας",
+    location: "Κάτω από το χωριό Φίλια, λίγο μετά από την ένωση του Λάδωνα με τον Τράγο",
+    year: "16ος αιώνας",
+    builder: "Άγνωστος",
+    description:
+      "Είναι μονότοξο με δύο ισομεγέθη ανακουφιστικά ανοίγματα δεξιά και αριστερά, καμαρωτά και με κάθετα τα πλαϊνά τους. Τα στηθαία είναι χτισμένα σχετικά πρόσφατα με τσιμεντόλιθους ενώ η βάση του είναι καλυμμένη με τσιμέντο, προφανώς από τους ντόπιους για την διευκόλυνση διάβασης των αγροτικών μηχανημάτων τους.",
+    links: 'Σχετικό άρθρο στο blog: <a href="http://agpelop.blogspot.com/2013/01/blog-post_27.html?spref=tw" target="_blank">Γεφύρι Τσερχοτάμπεη στο Λάδωνα</a>',
+  },
 };
 
-document.getElementById('bridge-select').addEventListener('change', function(e) {
-    console.log('Selection changed:', e.target.value); // Add this line
-    const detailsDiv = document.getElementById('bridge-details');
-    if (!e.target.value) {
-        detailsDiv.classList.add('hidden');
-        return;
-    }
+document.getElementById("bridge-select").addEventListener("change", function (e) {
+  console.log("Selection changed:", e.target.value);
+  const detailsDiv = document.getElementById("bridge-details");
+  if (!e.target.value) {
+    detailsDiv.classList.add("hidden");
+    return;
+  }
 
-    const bridge = bridgeData[e.target.value];
-    detailsDiv.innerHTML = `
+  const bridge = bridgeData[e.target.value];
+  detailsDiv.innerHTML = `
         <h4>${bridge.name}</h4>
-        <p><strong><i class="fas fa-map-marker-alt"></i> Νομός:</strong> ${bridge.county2}</p>
+        <p><strong><i class="fas fa-map-marker-alt"></i> Νομός:</strong> ${bridge.county}</p>
         <p><strong><i class="fas fa-map-marker-alt"></i> Δήμος:</strong> ${bridge.municipal}</p>
         <p><strong><i class="fas fa-water"></i> Ποτάμι:</strong> ${bridge.river}</p>
         <p><strong><i class="fas fa-map-marker-alt"></i> Θέση:</strong> ${bridge.location}</p>
@@ -45,11 +94,11 @@ document.getElementById('bridge-select').addEventListener('change', function(e) 
         <p><strong><i class="fas fa-info-circle"></i> Περιγραφή:</strong> ${bridge.description}</p>
         <p><strong><i class="fas fa-link"></i> Σύνδεσμοι:</strong> ${bridge.links}</p>
     `;
-    detailsDiv.classList.remove('hidden');
+  detailsDiv.classList.remove("hidden");
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    const detailsDiv = document.getElementById('bridge-details');
-    detailsDiv.classList.add('hidden');
-    document.getElementById('bridge-select').value = '';
+document.addEventListener("DOMContentLoaded", function () {
+  const detailsDiv = document.getElementById("bridge-details");
+  detailsDiv.classList.add("hidden");
+  document.getElementById("bridge-select").value = "";
 });
